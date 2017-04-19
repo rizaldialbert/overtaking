6e4d6a8f5ee915002996a5dc01cbfc2393ec7fb0
Fabian Immler <immler@in.tum.de>
2017-04-19 13:18:51 +0200

    inline code generation also here

--- Test_Code_Generation_Real_Approx.thy
+++ Test_Code_Generation_Real_Approx.thy
@@ -18,18 +18,16 @@ definition Realfract :: "integer \<Rightarrow> integer \<Rightarrow> real"
   where "Realfract p q = real_of_integer p / real_of_integer q"
 
 lemma [code]: "Ratreal r = (case (quotient_of r) of (x, y) \<Rightarrow>
-  Test_Code_Generation.Realfract (integer_of_int x) (integer_of_int y))"
+  Realfract (integer_of_int x) (integer_of_int y))"
   by (cases r)
      (simp add: Realfract_def quotient_of_Fract of_rat_rat
        real_of_integer_def)
 
-export_code segment_intersection in SML module_name Test file "code/segment_intersection.sml"
-
+ML \<open>val segment_intersection = @{code segment_intersection}\<close>
 ML \<open>
 val segment1 = ((0.0,0.0), (5.0,0.0));
 val segment2 = ((0.0,0.0), (5.0,1.0));
 \<close>
-ML_file "code/segment_intersection.sml"
-ML \<open>Test.segment_intersection segment1 segment2\<close>
+ML \<open>segment_intersection segment1 segment2\<close>
 
 end
